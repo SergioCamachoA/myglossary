9d101272660bcd9affc1fa8dc7e40c765f5ec681
Sergio Camacho <ser.a.camacho93@protonmail.com>
2021-07-05 14:31:54 -0500

    categories setup fixed general styling

--- src/pages/MostViewed.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const MostViewed = () => {
-    return (
-        <div>
-            <h1>Most Viewed</h1>
-        </div>
-    )
-}
--- src/pages/SingleCategory.js
+++ src/pages/SingleCategory.js
@@ -2,11 +2,26 @@ import { useParams, Link } from "react-router-dom"
 import React from "react"
 
 export function SingleCategory() {
-  let { name } = useParams()
+  const { name } = useParams()
+
+  const conceptsArray = JSON.parse(localStorage.getItem("mainList"))
+  const filteredArray = conceptsArray.filter((each) => each.category === name)
+
   return (
-    <div className="category">
-      <h1>{name}</h1>
+    <div className="category-page">
       <Link to="/">regrese uno</Link>
+      <div className="category-details">
+        <h1>{name}</h1>
+        <div className="category-specs">
+          {filteredArray.map((each, index) => {
+            return (
+              <Link key={index} to={`/${each.topic}`}>
+                <h2>{each.topic}</h2>
+              </Link>
+            )
+          })}
+        </div>
+      </div>
     </div>
   )
 }
